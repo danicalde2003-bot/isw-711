ff357aa5e77d37b26fbec6ec70ec6a610cfcbf0f
danicalde2003 <danicalde2003@gmail.com>
2026-02-12 20:38:33 -0600

    addding workshop 1

---
+++ workshop1/routes/routes.js
@@ -0,0 +1,68 @@
+const express = require('express');
+const router = express.Router();
+const Model = require('../models/model.js');
+
+// POST: Enviar datos a la base de datos
+router.post('/post', async (req, res) => {
+    const data = new Model({
+        name: req.body.name,
+        age: req.body.age
+    });
+
+    try {
+        const dataToSave = await data.save();
+        res.status(200).json(dataToSave);
+    } catch (error) {
+        res.status(400).json({ message: error.message });
+    }
+});
+
+// GET: Obtener todos los registros
+router.get('/getAll', async (req, res) => {
+    try {
+        const data = await Model.find();
+        res.json(data);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+});
+
+// GET por ID: Obtener un solo registro específico
+router.get('/getOne/:id', async (req, res) => {
+    try {
+        const data = await Model.findById(req.params.id);
+        res.json(data);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+});
+
+// PATCH: Actualizar datos por ID
+router.patch('/update/:id', async (req, res) => {
+    try {
+        const id = req.params.id;
+        const updatedData = req.body;
+        const options = { new: true };
+
+        const result = await Model.findByIdAndUpdate(
+            id, updatedData, options
+        );
+
+        res.send(result);
+    } catch (error) {
+        res.status(400).json({ message: error.message });
+    }
+});
+
+// DELETE: Eliminar un registro por ID
+router.delete('/delete/:id', async (req, res) => {
+    try {
+        const id = req.params.id;
+        const data = await Model.findByIdAndDelete(id);
+        res.send(`Documento con el nombre ${data.name} ha sido eliminado.`);
+    } catch (error) {
+        res.status(400).json({ message: error.message });
+    }
+});
+
+module.exports = router;
